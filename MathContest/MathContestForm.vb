16ad7a2b4b68dc58eb95a67c418e1b7bb5feaaf2
Jamison Burton <jamison@burtontalk.com>
2022-04-06 08:18:30 -0600

    Added descriptions.

--- MathContest/MathContestForm.vb
+++ MathContest/MathContestForm.vb
@@ -6,19 +6,23 @@
 
 Public Class MathContestForm
 
+    'Generates a random number
     Function RandomInteger() As Integer
         Randomize()
         Dim value As Integer = CInt(Int((10 * Rnd()) + 1))
         Return value 'Outputs a random value between 1 and 10
     End Function
+    'Enables StudentAnswerTextbox and generates random numbers
     Private Sub RadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles AddRadioButton.CheckedChanged, SubtractRadioButton.CheckedChanged, MultiplyRadioButton.CheckedChanged, DivideRadioButton.CheckedChanged
         FirstNumberTextBox.Text = RandomInteger()
         SecondNumberTextBox.Text = RandomInteger()
         StudentAnswerTextBox.Enabled = True
     End Sub
+    'Closes the program
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
+    'Verifies that a name was input after focus is moved off the NameTextbox
     Private Function NameTextBox_Leave(sender As Object, e As EventArgs) Handles NameTextBox.Leave
         Dim valid As Boolean = False
 
@@ -31,10 +35,10 @@ Public Class MathContestForm
 
         Return valid
     End Function
-
     Private Sub AgeTextBox_Leave(sender As Object, e As EventArgs) Handles AgeTextBox.Leave
         validateAge()
     End Sub
+    'Checks the age is within 7-11 and is a number
     Function validateAge() As Boolean
         Dim valid As Boolean = False
         Dim age As String
@@ -62,6 +66,7 @@ Public Class MathContestForm
     Private Sub GradeTextBox_Leave(sender As Object, e As EventArgs) Handles GradeTextBox.Leave
         validateGrade()
     End Sub
+    'Checks the grade is between 1st-4th grade and is a number
     Function validateGrade() As Boolean
         Dim valid As Boolean = False
         Dim grade As String
@@ -84,7 +89,7 @@ Public Class MathContestForm
         End If
         Return valid
     End Function
-
+    'Checks for answer, name, grade, and age before accepting submissions
     Private Sub StudentAnswerTextBox_TextChanged(sender As Object, e As EventArgs) Handles StudentAnswerTextBox.TextChanged
         If StudentAnswerTextBox.Text <> "" And NameTextBox.Text <> "" And validateAge() = True And validateGrade() = True Then
             SubmitButton.Enabled = True
@@ -94,6 +99,7 @@ Public Class MathContestForm
             SummaryButton.Enabled = False
         End If
     End Sub
+    'Compares answer to the correct value and displays message, also adds to tally of correct answers
     Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
         Dim input As Integer = CInt(StudentAnswerTextBox.Text)
         Dim answer = EnterAnswer()
@@ -111,6 +117,7 @@ Public Class MathContestForm
         SummaryButton.Enabled = True
 
     End Sub
+    'Switches math function based on RadioButtons
     Function EnterAnswer() As Integer
         Dim answer As Integer
 
@@ -126,6 +133,7 @@ Public Class MathContestForm
 
         Return answer
     End Function
+    'Generates and displays a running tally for the contest
     Sub Tally(Optional input As Boolean = False, Optional clear As Boolean = False, Optional write As Boolean = False)
         Static results As Integer = 0
         Static total As Integer = 0
@@ -152,6 +160,7 @@ Public Class MathContestForm
         End If
 
     End Sub
+    'Resets the form for a new contestant
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         Tally(, True,)
         NameTextBox.Text = ""
@@ -162,6 +171,7 @@ Public Class MathContestForm
         SecondNumberTextBox.Text = RandomInteger()
         AddRadioButton.Checked = True
     End Sub
+    'Displays the tally of correct answers to total
     Private Sub SummaryButton_Click(sender As Object, e As EventArgs) Handles SummaryButton.Click
         Tally(False, False, True)
     End Sub
